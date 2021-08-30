e341e631895e84c1d66073ed3e69b9c308bc7f03
smyrmnsr <63547955+smyrmnsr@users.noreply.github.com>
2021-08-31 00:22:36 +0300

    Button component created

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+export const Button = styled(Link)`
+    background: ${({ primary }) => (primary ? '#000d1a' : 
+    'CD853F')}; 
+`;
